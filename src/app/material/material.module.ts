5baa2ce247e3e20c27ecdaa660e0c6b2cc867a9d
patrykkrol98 <patryk.krol.98@gmail.com>
2022-08-01 18:48:08 +0200

    material module added

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+const MaterialComponents = [
+  MatToolbarModule,
+]
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
