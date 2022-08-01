f1ceb44aa8a8f7d6aeaa928a149279fb1bdcbb2b
patrykkrol98 <patryk.krol.98@gmail.com>
2022-08-01 18:56:38 +0200

    routing, toolbar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'feed', component: FeedComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'feed' },
+  { path: '**', component: PathNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { FeedComponent } from './feed/feed.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PathNotFoundComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
