9862b0961a3097fc078a8f13db97f0fe7463cd36
patrykkrol98 <patryk.krol.98@gmail.com>
2022-08-01 19:23:19 +0200

    Highlight service added

---
+++ src/app/highlight.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs'
+import { IHighlight } from './highlight';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HighlightService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getHighlights(): Observable<IHighlight[]> {
+   return this.httpClient.get<{response: IHighlight[]}>('./assets/response.json').pipe(
+    map(res => res.response)
+   )
+  }
+}
