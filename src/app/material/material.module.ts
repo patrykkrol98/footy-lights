819a51ff3783d22357f92f6517e304d3a5551795
patrykkrol98 <patryk.krol.98@gmail.com>
2022-08-03 23:15:29 +0200

    new material colors and material components added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+
 const MaterialComponents = [
   MatToolbarModule,
+  MatCardModule,
+  MatDividerModule,
+  MatIconModule,
 ]
 
 @NgModule({
